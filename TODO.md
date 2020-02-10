47276452f885600bcd1f55444895f1cf688b0835
Nokko <nokkonokkonokkonokko@gmail.com>
2020-02-10 13:41:12 -0800

    Improve server CLI, improve client CSS

---
+++ TODO.md
@@ -0,0 +1,29 @@
+### Tech Stack/Libraries:
+* shim for nicer chaining?
+* socket.io <3 cycle.js?
+* enum message types? 
+  * '9' is lighter than 'chat' is lighter than 'please-nick'
+
+### Features
+* nick collision
+* fix pleasenick bug
+  * reproduce pleasenick bug?
+* put more things in grid 
+  * nickname/text container
+  * compose/send container
+* sessions
+  * remember thy nick
+* message history
+  * redis, memcached, JSON, whatever
+* in-chat hyperlinks 
+  * maybe embeds? 0.0
+* private messages
+* MoTD
+* operator stuff?
+* channels
+* ratelimit(s)
+  * text-per-message limit
+  * anti-link?
+  * /shrug
+* `who` command/feature
+* Twemoji!
--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,26 @@
-let socket = io() 
+// "Use `const` whenever possible. It is always possible."
+// - Simon Peyton Jones
+const socket = io() 
 
 // Listen for message box submissions:
 document.querySelector('form').addEventListener('submit', function(e){
-    e.preventDefault();    // Stop form from "actually" submitting
-    msg = document.querySelector('#m').value
-    
-    if (msg.startsWith('!')){
-        parseCommand(msg)
-    } else {
-        socket.emit('chat', msg)
-        console.log('chat:', msg)   
-    }
+	e.preventDefault();    // Stop form from "actually" submitting
+	msg = document.querySelector('#m').value
+	
+	// Either do command stuff on the clientside before doing whatever is needed from the command,
+	// *or* just send a `chat` event to the server
+	if (msg.startsWith('!')){
+		parseCommand(msg)
+	} else {
+		socket.emit('chat', msg)
+		console.log('chat:', msg)   
+	}
 
-    clearMbox()
+	clearMbox()
 })
 // Clear the message box. (That is, the field where you type a message to send.)
 function clearMbox() {
-    document.querySelector('#m').value = '' // There may be a bug here.
+	document.querySelector('#m').value = '' // There may be a bug here.
 }
 
 /**
@@ -24,25 +28,25 @@ function clearMbox() {
  * @param  {String} msg - A command message, starting with an exclamation point.
  */
 function parseCommand(msg) {
-    // #region Commands that are later referenced in `commands` object:
-    
-    function renick(args) {
-        newNick = args.join(' ') // we gotta send a string, not an array
-        console.log('renicking to', newNick)
-        socket.emit('new-nick', newNick)
-    }
-    
-    // #endregion
+	// #region Commands that are later referenced in `commands` object:
+	
+	function renick(args) {
+		newNick = args.join(' ') // we gotta send a string, not an array
+		console.log('renicking to', newNick)
+		socket.emit('new-nick', newNick)
+	}
+	
+	// #endregion
 
-    const commands = {
-        'nick': renick,
-        'renick': renick, // aliases, eh
-    }
-    let args = msg.slice(1).split(' ')
-    console.log(args[0], args[0] in commands)
-    if (args[0] in commands){
-        commands[args[0]](args.slice(1))
-    }
+	const commands = {
+		'nick': renick,
+		'renick': renick, // aliases, eh
+	}
+	let args = msg.slice(1).split(' ')
+	console.log(args[0], args[0] in commands)
+	if (args[0] in commands){
+		commands[args[0]](args.slice(1))
+	}
 }
 
 // #region CHAT STUFF
@@ -56,33 +60,34 @@ function parseCommand(msg) {
  * @param  {Boolean} isSuper - whether the message should be rendered with the 'superchat' CSS class
  */
 function genericChat(nick, msg, isSuper) {
-    
-    let msgNode = document.createElement('li')
-    let shouldScroll = false
-    const mBox = document.querySelector('#messages')
+	
+	let msgNode = document.createElement('li')
+	let shouldScroll = false
+	const mBox = document.querySelector('#messages')
 
-    if (nick) {
-        let nickNode = document.createElement('strong')
-        nickNode.innerText = nick
-        msgNode.appendChild(nickNode).classList.add('nick')
-    }
-    
-    let textNode = document.createElement('span')
-    textNode.innerText = msg
-    
-    if (isSuper)
-        textNode.classList.add('superchat')
-    
-    msgNode.appendChild(textNode)
-    
-    // check if we're scrolled to the bottom
-    if (mBox.scrollHeight - mBox.clientHeight <= mBox.scrollTop + 1)
-        shouldScroll = true
-    
-    mBox.appendChild(msgNode)
-    
-    if (shouldScroll)
-        msgNode.scrollIntoView()
+	if (nick) {
+		let nickNode = document.createElement('strong')
+		nickNode.innerText = nick
+		msgNode.appendChild(nickNode).classList.add('nick')
+	}
+	
+	let textNode = document.createElement('span')
+	textNode.classList.add('msg')
+	textNode.innerText = msg
+	
+	if (isSuper)
+		textNode.classList.add('superchat')
+	
+	msgNode.appendChild(textNode)
+	
+	// check if we're scrolled to the bottom
+	if (mBox.scrollHeight - mBox.clientHeight <= mBox.scrollTop + 1)
+		shouldScroll = true
+	
+	mBox.appendChild(msgNode)
+	
+	if (shouldScroll)
+		msgNode.scrollIntoView()
 }
 
 
@@ -100,29 +105,29 @@ socket.on('serverchat', serverChat)
 
 // We make a connection.
 socket.on('connection', (connect) => {
-    console.log(connect)
+	console.log(connect)
 });
 
 // Someone's nick updates. If oldNick is '' (i.e. falsy), it's a new user joining
 socket.on('new-nick', (oldNick, newNick) => {
-    console.log('new-nick')
-    console.log(oldNick, 'renicked to', newNick)
-    if (oldNick) {
-        serverChat(`${oldNick} has renicked to ${newNick}.`)
-    } else {
-        serverChat(`${newNick} has joined the chat!`)
-    }
+	console.log('new-nick')
+	console.log(oldNick, 'renicked to', newNick)
+	if (oldNick) {
+		serverChat(`${oldNick} has renicked to ${newNick}.`)
+	} else {
+		serverChat(`${newNick} has joined the chat!`)
+	}
 })
 
 // Server asks us to provide a nickname. 
 // Happens on first join and possibly on admin discretion.
 socket.on('nick-please', () => {
-    console.log('asked for nick')
-    let newNick = ''
-    while (!newNick) {
-        newNick = prompt('Please enter a new nickname!', 'Somebody')
-    }
-    
-    socket.emit('new-nick', newNick)
-    console.log('sent new nick', newNick)
+	console.log('asked for nick')
+	let newNick = ''
+	while (!newNick) {
+		newNick = prompt('Please enter a new nickname!', 'Somebody')
+	}
+	
+	socket.emit('new-nick', newNick)
+	console.log('sent new nick', newNick)
 })
