1c0a6bfb515ed18fc36d2dde9bc27971dabc0a17
Nokko <nokkonokkonokkonokko@gmail.com>
2020-02-09 20:11:13 -0800

    Add initial work

---
+++ assets/script.js
@@ -0,0 +1,97 @@
+let socket = io() 
+
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();    //stop form from submitting
+    msg = document.querySelector('#m').value
+    
+    if (msg.startsWith('!')){
+        parseCommand(msg)
+    } else {
+        socket.emit('chat', msg)
+        console.log('chat:', msg)   
+    }
+    
+    // clear the field (too early?)
+    clearMbox()
+})
+
+function renick(args) {
+    newNick = args.join(' ')
+    console.log('renicking to', newNick)
+    socket.emit('new-nick', newNick)
+}
+
+
+function parseCommand(msg) {
+//    console.log('parsing command', msg)
+    const commands = {
+        'nick': renick
+    }
+    let args = msg.slice(1).split(' ')
+    console.log(args[0], args[0] in commands)
+    if (args[0] in commands){
+        
+        commands[args[0]](args.slice(1))
+    }
+    
+}
+
+function genericChat(nick, msg, isSuper) {
+    let msgNode = document.createElement('li')
+
+    if (nick) {
+        let nickNode = document.createElement('strong')
+        nickNode.innerText = nick
+        msgNode.appendChild(nickNode).classList.add('nick')
+    }
+    
+    let textNode = document.createElement('span')
+    textNode.innerText = msg
+    
+    if (isSuper) {
+        textNode.classList.add('superchat')
+    }
+    
+    msgNode.appendChild(textNode)
+    
+        
+    document.querySelector('#messages').appendChild(msgNode)
+}
+
+const serverChat = (msg) => { console.log('sChat:', msg); genericChat('', msg, true) }
+const userChat = (nick, msg) => { console.log('uChat:', msg); genericChat(nick, msg, false) }
+
+
+socket.on('chat', userChat)
+
+socket.on('serverchat', serverChat)
+
+
+socket.on('connection', (connect) => {
+    console.log(connect)
+});
+
+socket.on('new-nick', (oldNick, newNick) => {
+    console.log('new-nick')
+    console.log(oldNick, 'renicked to', newNick)
+    if (oldNick) {
+        serverChat(`${oldNick} has renicked to ${newNick}.`)
+    } else {
+        serverChat(`${newNick} has joined the chat!`)
+    }
+})
+
+socket.on('nick-please', () => {
+    console.log('asked for nick')
+    let newNick = ''
+    while (!newNick) {
+        newNick = prompt('Please enter a new nickname!', 'Somebody')
+    }
+    
+    socket.emit('new-nick', newNick)
+    console.log('sent new nick', newNick)
+})
+
+function clearMbox() {
+    document.querySelector('#m').value = '' // There may be a bug here.
+}
---
+++ index.js
@@ -0,0 +1,60 @@
+let express = require('express')
+let app = express()
+let http = require('http').createServer(app)
+let io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/assets/index.html')
+})
+
+app.use(express.static('assets')) // Scripts, Stylesheets & Such.
+
+nicks = {} // id -> nick
+
+io.on('connection', function(socket){
+    console.log('[CONN]', 'user connected with IP:', socket.handshake.address, 'id:', socket.id)
+    
+    // add an entry to the nick table
+    nicks[socket.id] = ''
+    
+    // ask for nick
+    socket.emit('nick-please')
+    socket.emit('serverchat', 'Change nicknames like so: !nick <new nickname>')
+    
+    socket.on('new-nick', (newNick) => {
+        let oldNick = nicks[socket.id]
+        
+        console.log('[INFO]', socket.id, 'tries to re-nick from', oldNick, 'to', newNick)
+        
+        if (newNick) { // not empty; TODO: validate things
+            nicks[socket.id] = newNick
+            io.emit('new-nick', oldNick, newNick)
+            console.log('[INFO]', socket.id, 'successfully renicked')
+        } else {
+            socket.emit('nick-please')
+        }
+    })
+    
+
+    socket.on('disconnect', function() {
+        console.log('[DCON] user id', socket.id,'disconnected')
+        delete nicks[socket.id] // cleanup nicks
+    })
+    socket.on('chat', function(msg){
+        // only send messages that come from nicked users
+        nick = nicks[socket.id]
+        if (nick) {
+            console.log(`[CHAT] ${nick}: ${msg}`)
+            io.emit('chat', nick, msg) // re-emit, to everyone including sender
+        
+        // ask unnicked users for a nickname, log their crying
+        } else {
+            console.log(`[NONICK]: ${socket.id.slice(0,5)} ${msg}`)
+            socket.emit('nick-please')
+        }
+    })
+})
+
+http.listen(3000, () => {
+    console.log('listening on port 3000')
+})
